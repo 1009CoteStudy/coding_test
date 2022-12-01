f34fc514ee17118520f3d549b0a96ac428a57299
Yun-Jehyeok <dbswpgur2@naver.com>
2022-12-01 10:45:13 +0900

    Implement

--- Chap12_Implement/치킨배달.py
+++ Chap12_Implement/치킨배달.py
@@ -19,23 +19,21 @@ for i in range(N):
 
 min_distance = 1e9
 
-for i in range(1, M + 1): # 치킨집 1~M개를 선택
-    for chicken in list(itertools.combinations(chickens, i)): # 치킨집 i개를 선택하는 모든 조합에 대해
-        distance = 0
+for chicken in list(itertools.combinations(chickens, M)): # 치킨집 i개를 선택하는 모든 조합에 대해
+    distance = 0
+    
+    for j in range(N):
+        for k in range(N):
+            if maps[j][k] == 1: # 각각 집에서
+                min_dis = abs(j - chicken[0][0]) + abs(k - chicken[0][1]) # 치킨 조합의 첫 번째 치킨집 까지의 거리가 최소라고 해놓고
+                for data in chicken: # 치킨 조합의 모든 치킨집에 대해 집에서 치킨집 까지의 최소 거리를 구하고
+                    tmp_dis = abs(j - data[0]) + abs(k - data[1])
+                    if tmp_dis < min_dis:
+                        min_dis = tmp_dis
 
-        for j in range(N):
-            for k in range(N):
-                if maps[j][k] == 1: # 각각 집에서
-                    min_dis = abs(j - chicken[0][0]) + abs(k - chicken[0][1]) # 치킨 조합의 첫 번째 치킨집 까지의 거리가 최소라고 해놓고
+                distance += min_dis # 최소 거리를 구해 거리에 더해준다.
 
-                    for data in chicken: # 치킨 조합의 모든 치킨집에 대해 집에서 치킨집 까지의 최소 거리를 구하고
-                        tmp_dis = abs(j - data[0]) + abs(k - data[1])
-                        if tmp_dis < min_dis:
-                            min_dis = tmp_dis
-
-                    distance += min_dis # 최소 거리를 구해 거리에 더해준다.
-
-        if distance < min_distance:
-            min_distance = distance
+    if distance < min_distance:
+        min_distance = distance
 
 print(min_distance)
