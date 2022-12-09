3598794026317bfc3ee1e4f7cc7231c37df83001
Yun-Jehyeok <dbswpgur2@naver.com>
2022-12-09 09:58:47 +0900

    DFS-BFS

--- test2.py
+++ test2.py
@@ -1,41 +1,10 @@
+import math
+
 N = int(input())
 
-datas = []
+flag = int(math.sqrt(N))
 
-for _ in range(N):
-    data = int(input())
-    datas.append(data)
+cnt = 0
 
-num_stack = []
-command_stack = []
-
-for i in range(1, N + 1):
-    if i != datas[0]:
-        num_stack.append(i)
-        command_stack.append('+')
-    else:
-        num_stack.append(i)
-        command_stack.append('+')
-
-        if len(datas) > 0:
-            cur = datas[0]
-            num_stack.append(datas.pop(0))
-            command_stack.append('-')
-
-            while len(datas) > 0 and datas[0] <= cur:
-                if num_stack[-1] != datas[0]:
-                    break
-
-                cur = datas[0]
-                datas.pop(0)
-                command_stack.append('-')
-
-    if len(datas) == 0:
-        break
-
-if len(datas) == 0:
-    for data in command_stack:
-        print(data)
-
-else:
-    print('NO')
+for i in range(flag, 0, -1):
+    if 
