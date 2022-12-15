193ecfcd9855de7aa49af1bdf555809be4e681d3
Rhode <98301931+hodoovely@users.noreply.github.com>
2022-12-15 15:58:41 +0900

    python study 221215

--- ch13 DFSBFS/18 괄호 변환.py
+++ ch13 DFSBFS/18 괄호 변환.py
@@ -1,2 +1,33 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60058
 
+# 정답보고 풀었음
+
+p = input()
+
+def checkCorrect(x):  # 이해가 안 됨
+    count = 0
+    for i in range(len(x)-1):
+        if x[i] == '(':
+            count += 1
+            print(i, count)
+        elif x[i] == ')':
+            if count == 0:
+                return False
+            count -= 1
+            print(i, count)
+    return True
+
+
+def devide(x):
+    for i in range(2, len(x), 2):
+        u = p[0:i]
+        v = p[i:len(p)]
+        print(u, v)
+
+
+if checkCorrect(p) == True:
+    print('True')
+
+devide(p)
+
+print(p.count('('))
